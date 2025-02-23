56c05f4936f9777423ebdb04dedf893eb4e3811d
Jeongmin Sohn <jmsohn724@gmail.com>
2025-02-23 00:00:28 -0500

    EDIT: created Shelter List

--- src/components/pages/ShelterPage.tsx
+++ src/components/pages/ShelterPage.tsx
@@ -1,7 +1,51 @@
 import React from "react";
-import { Container, TextField, InputAdornment, Box } from "@mui/material";
+import {
+  Container,
+  TextField,
+  InputAdornment,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Avatar,
+  Paper,
+} from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+import FireTruckIcon from "@mui/icons-material/LocalFireDepartment";
+import PoliceIcon from "@mui/icons-material/LocalPolice";
+const shelters = [
+  {
+    name: "Mather Hospital",
+    distance: "0.5 miles",
+    phone: "+1-202-555-0114",
+    icon: <LocalHospitalIcon />,
+  },
+  {
+    name: "Port Jefferson Fire Department",
+    distance: "0.7 miles",
+    phone: "+1-202-555-0114",
+    icon: <FireTruckIcon />,
+  },
+  {
+    name: "A Police Department",
+    distance: "1.2 miles",
+    phone: "+1-202-555-0114",
+    icon: <PoliceIcon />,
+  },
+  {
+    name: "B Police Department",
+    distance: "1.5 miles",
+    phone: "+1-202-555-0114",
+    icon: <PoliceIcon />,
+  },
+  {
+    name: "C Police Department",
+    distance: "1.7 miles",
+    phone: "+1-202-555-0114",
+    icon: <PoliceIcon />,
+  },
+];
 const ShelterPage: React.FC = () => {
   return (
     <Container>
@@ -26,6 +70,29 @@ const ShelterPage: React.FC = () => {
           },
         }}
       />
+      <List>
+        {shelters.map((shelter, index) => (
+          <Paper elevation={3} sx={{ borderRadius: "16px", mb: 1 }} key={index}>
+            <ListItem component="div">
+              <ListItemAvatar>
+                <Avatar sx={{ backgroundColor: "#980000", color: "white" }}>
+                  {shelter.icon}
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={shelter.name}
+                secondary={
+                  <>
+                    Distance: {shelter.distance}
+                    <br />
+                    {shelter.phone}
+                  </>
+                }
+              />
+            </ListItem>
+          </Paper>
+        ))}
+      </List>
     </Container>
   );
 };
